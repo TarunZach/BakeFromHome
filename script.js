9ab3e52784989db7ce0001b9104ab37fe725b49e
TarunZach <tzacharias.97@gmail.com>
2020-09-21 21:51:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function toggleSidebar() {
+
+    let hamburger = {
+        nav: document.querySelector('#nav'),
+        navToggle: document.querySelector('.nav-toggle'),
+
+        initialize() {
+            this.navToggle.addEventListener('click',
+                () => { this.toggle(); });
+        },
+
+        toggle() {
+            this.navToggle.classList.toggle('expanded');
+            this.nav.classList.toggle('expanded');
+        },
+    };
+
+    hamburger.initialize();
+
+};
+
+/*function toggleSidebar()
+{
+	document.getElementById("sidebar").classList.toggle('active');
+}*/
+
+$('.js-input').keyup(function () {
+    if ($(this).val()) {
+        $(this).addClass('not-empty');
+    } else {
+        $(this).removeClass('not-empty');
+    }
+});
+
+
+
+
